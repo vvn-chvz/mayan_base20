a17b257b1eeea524b611d8c81ad9ddab1e862573
vchavez <vivchavez1220@gmail.com>
2020-06-30 13:05:53 -0500

    Updates

---
+++ precache-manifest.754a69aaec1738f64cdb0fac88b8a7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c2b092681f21ef51bc678d4c926269",
+    "url": "/mayan_base20/index.html"
+  },
+  {
+    "revision": "013b4ffffd59cbc2917f",
+    "url": "/mayan_base20/static/css/main.cefc018a.chunk.css"
+  },
+  {
+    "revision": "928860076884fcb01677",
+    "url": "/mayan_base20/static/js/2.4465d0f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mayan_base20/static/js/2.4465d0f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013b4ffffd59cbc2917f",
+    "url": "/mayan_base20/static/js/main.4f3967d7.chunk.js"
+  },
+  {
+    "revision": "a44c8c915d68327c3679",
+    "url": "/mayan_base20/static/js/runtime-main.5d8bfbd8.js"
+  }
+]);

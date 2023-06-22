3f9ccae4099298e1484309d52b7eb9d098d2af45
Vivian Chavez <vivchavez1220@gmail.com>
2023-06-22 16:31:50 -0400

    Import mayan numeral font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Wrap from './component/Wrap';
 
+// Mayan Numeral Unicode
+import "./fonts/NotoSansMayanNumerals-Regular.ttf"
 
 class App extends React.Component {
 
